23d683c1f1bc3b56906c9cde2d9e5a569b6ad99b
Abdulrazak Darwish <105983363+Rizeq-D@users.noreply.github.com>
2024-01-07 16:11:56 +0100

    added new students

--- inserts-studentsTable.sql
+++ inserts-studentsTable.sql
@@ -1,3 +1,5 @@
 INSERT INTO students(first_name, last_name, phone, email, homeroom_number, grad_year)
 
-	VALUES('Mark', 'Watney', '777-555-1234', 'Does not have an email', 5, 2035);
+	-- VALUES('Mark', 'Watney', '777-555-1234', 'Does not have an email', 5, 2035);   
+	   VALUES('Malek', 'Ra', '777-555-3254', 'malek@gmail.com', 4, 2035, '10'),
+	         ('Rana', 'Ma', '777-111-0921', 'rana@gmail.com', 2, 2035, '10');
