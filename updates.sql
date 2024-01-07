7b48f16e8d02688960f779d3433acead83431792
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2024-01-07 16:29:45 +0100

    added new updates and tests

--- updates.sql
+++ updates.sql
@@ -6,8 +6,6 @@ ADD COLUMN department VARCHAR(50);
 ALTER TABLE students
 ADD COLUMN class_number VARCHAR(20); 
 
-*/
-
 SELECT teacher_id, first_name, last_name,
 
 CASE 
@@ -18,3 +16,27 @@ CASE
 END AS Waffel_party
 
 FROM teachers;
+
+SELECT (
+	
+	SUM(CASE WHEN class_number = '10' THEN 1 ELSE 0 END)/
+	SUM(CASE WHEN class_number = '5' THEN 1 ELSE 0 END)
+) AS clesses_ratio
+
+FROM students
+
+DELETE FROM students
+WHERE class_number = '5';
+
+*/
+
+SELECT (
+	
+	SUM(CASE WHEN class_number = '10' THEN 1 ELSE 0 END)/
+	NULLIF(SUM(CASE WHEN class_number = '5' THEN 1 ELSE 0 END),0)
+) AS clesses_ratio
+
+FROM students;
+
+
+
