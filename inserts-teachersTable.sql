de7ed03de31836b0b96ca828c49f13a3dd55d4d8
Abdulrazak Darwish <105983363+Rizeq-D@users.noreply.github.com>
2024-01-07 14:18:53 +0100

    modified inserts-teachersTable.sql

--- inserts-teachersTable.sql
+++ inserts-teachersTable.sql
@@ -3,21 +3,21 @@ INSERT INTO teachers(first_name, last_name, phone, email, homeroom_number, depar
 	-- VALUES('Jonas', 'Salk', '777-555-4321', 'jsalk@school.org', 5, 'Biology')
 	VALUES
 	   ('Ahmed', 'Shawa', '777-444-9874', 'ahmed@school.org', 4, 'Math'),
-       ('Jalta', 'Mouhammed', '777-233-6234', 'jalta@school.org', 7, 'Art'),
-       ('Ammar', 'Hex', '777-555-2351', 'ammar@school.org', 8, 'Sport'),
-       ('Yaser', 'Saleh', '777-767-4354', 'saleh@school.org', 11, 'Biology'),
-       ('John', 'Smith', '777-999-9705', 'john@school.org', 2, 'History'),
-       ('Amanda', 'Kiera', '777-333-0430', 'amanda@school.org', 18, 'Music'),
-       ('Selina', 'Gari', '777-555-1014', 'selina@school.org', 1, 'Physics'),
-       ('Yumna', 'Haddad', '777-222-3271', 'yumna@school.org', 23, 'Chemics'),
-       ('Yashar', 'Demir', '777-832-0903', 'yashar@school.org', 45, 'English'),
-       ('Yousof', 'Dar', '777-732-8371', 'yousof@school.org', 83, 'Arabic'),
-       ('Fatima', 'Bari', '777-342-8189', 'fatima@school.org', 65, 'Math'),
-       ('Darya', 'Berik', '777-832-5546', 'darya@school.org', 56, 'Geography'),
-       ('Yaser', 'Dayar', '777-000-7888', 'yaser@school.org', 89, 'Sport'),
-       ('Sahem', 'Co', '777-090-1231', 'sahem@school.org', 33, 'Philosophy'),
-       ('Aisha', 'Amir', '777-832-0040', 'aisha@school.org', 22, 'Physics'),
-       ('Amira', 'Tami', '777-373-8711', 'amira@school.org', 93, 'Geography');
+           ('Jalta', 'Mouhammed', '777-233-6234', 'jalta@school.org', 7, 'Art'),
+       	   ('Ammar', 'Hex', '777-555-2351', 'ammar@school.org', 8, 'Sport'),
+           ('Yaser', 'Saleh', '777-767-4354', 'saleh@school.org', 11, 'Biology'),
+           ('John', 'Smith', '777-999-9705', 'john@school.org', 2, 'History'),
+           ('Amanda', 'Kiera', '777-333-0430', 'amanda@school.org', 18, 'Music'),
+           ('Selina', 'Gari', '777-555-1014', 'selina@school.org', 1, 'Physics'),
+           ('Yumna', 'Haddad', '777-222-3271', 'yumna@school.org', 23, 'Chemics'),
+           ('Yashar', 'Demir', '777-832-0903', 'yashar@school.org', 45, 'English'),
+           ('Yousof', 'Dar', '777-732-8371', 'yousof@school.org', 83, 'Arabic'),
+           ('Fatima', 'Bari', '777-342-8189', 'fatima@school.org', 65, 'Math'),
+           ('Darya', 'Berik', '777-832-5546', 'darya@school.org', 56, 'Geography'),
+           ('Yaser', 'Dayar', '777-000-7888', 'yaser@school.org', 89, 'Sport'),
+           ('Sahem', 'Co', '777-090-1231', 'sahem@school.org', 33, 'Philosophy'),
+           ('Aisha', 'Amir', '777-832-0040', 'aisha@school.org', 22, 'Physics'),
+           ('Amira', 'Tami', '777-373-8711', 'amira@school.org', 93, 'Geography');
 
 
 
